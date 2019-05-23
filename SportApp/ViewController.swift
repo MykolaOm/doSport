db4ef90bafc16bc8f2f4f7699fe1d1210dc6e9c3
Микола Омельянов <mykola.omelyanov@gmail.com>
2019-05-23 10:53:11 +0300

    Initial Commit

---
+++ SportApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportApp
+//
+//  Created by Nikolas Omelianov on 5/23/19.
+//  Copyright © 2019 Nikolas Omelianov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
