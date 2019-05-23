9de6c676613b3b8c17383636845d76dbb38b5577
Микола Омельянов <mykola.omelyanov@gmail.com>
2019-05-23 12:06:12 +0300

    json tests

--- SportApp/ViewController.swift
+++ SportApp/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var doSomeEx : ExersiceEnumeration?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        doSomeEx = .situp
+        let exFact = ExFactory()
+        let ex = exFact.createExersice(type: doSomeEx!)
+        ex.startExersice()
+        print(ex.name)
+        var memento = ExercisesMemento()
+        print(memento.toJSON())
+//        var str = memento.toJSON()
+//        let jsonEncoder = JSONEncoder()
+//        let jsonData = try? jsonEncoder.encode(memento)
+//        print("ip")
+//        print(jsonData)
+//        print("ip")
+//        let jsonString = String(data: jsonData!, encoding: .utf8)
+//        print(jsonString)
+//        let decoder = JSONDecoder()
+//        let other = try? decoder.decode(ExercisesMemento.self, from: jsonData!)
+//        print(other)
+//        let mem = try JSONSerialization.jsonObject(with: memento, options: [])
+//        do {
+//            let jsonD = try JSONEncoder().encode(memento)
+//            print(jsonD)
+//            let jsonString = String(data: jsonD, encoding: .utf8)
+//            print(jsonD)
+//            let decoded = try JSONDecoder().decode(ExercisesMemento.self, from: jsonD)
+//            print(decoded)
+//        } catch {print(error)}
+//        print(mem)
+        //        doAll()
     }
-
-
+    //    func doAll() {
+    //        ExersiceEnumeration.allCases.forEach {
+    //            print(ExFactory().createExersice(type: $0).name)
+    //            ExFactory().createExersice(type: $0).startExersice()
+    //        }
+    //
+    //    }
+//    func startTraining(){
+//        var memento = ExercisesMemento()
+//        excecute()
+//    }
+//    func execute(ex: SportExersices){
+//        ex.startExersice()
+//        counter += 1
+//    }
 }
 
